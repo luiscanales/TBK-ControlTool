2d379b588ff7639a8e86b9f55244eb9e56b66a0a
Maximiliano <maxisaezv@gmail.com>
2018-10-16 19:53:44 -0300

    Branch max

--- src/app.js
+++ src/app.js
@@ -15,18 +15,20 @@ mongoose.connect('mongodb://localhost/tbk-controltool')
 const indexRoutes = require('./routes/index');
 
 //configuraciones
-app.set('port',process.env.PORT || 3000);
-app.set('views',path.join(__dirname + '/views'));
-app.set('view engine','ejs');
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine', 'ejs');
 
 //middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 //rutas
-app.use('/',indexRoutes);
+app.use('/', indexRoutes);
 
 //starting the server
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
 });
+
+//prueba branch
